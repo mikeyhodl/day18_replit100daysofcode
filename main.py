211e9bb069c6a375152c31c0301c892d5326d223
mikey <mike.owino40@yahoo.com>
2022-10-08 19:30:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Welcome to Guess the Number.")
+print()
+print("Guess a number between 1 and 1,000,000 and I will tell you if you are too low, too high, or get it correct.")
+print()
+print("Let's play!")
+
+correct_number = 2300
+attempt = 1
+zero = 0
+
+while True:
+  user_guess = float(input("Pick a number between 1 and 1,000,000: "))
+  if user_guess <= correct_number:
+    print("That number is too low. Try again!")
+    attempt += 1
+  elif user_guess > correct_number:
+    print("That number is too high. Try again!")
+    attempt += 1
+    continue
+  elif user_guess == correct_number:
+    print("You are a winner! 🥳🥳🥳🥳🥳🥳")
+    break 
+  elif user_guess <= zero:
+    print("Now we'll never know what the answer is …")
+    exit()
+  else:
+    print("That is not a number I recognize.")
+print("It took you", attempt, "attempt(s) to get the correct answer.")
